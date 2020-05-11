8d8654367cca9e3fe0d76f2ecdc311e118326d3a
CreamyMilk <creamym254@gmail.com>
2020-05-11 04:58:53 -0400

    Added /lipa endpoint💰 added a POST route and implemented the use of environment 🤫variables

--- Server.js
+++ Server.js
@@ -1,19 +1,17 @@
 const express = require('express');
-const fetch =require('node-fetch');
-
+const fetch = require('node-fetch');
+const moment = require('moment'); //Package to generate timestamp
 const app = express();
-require('dotenv').config();
+require('dotenv').config(); //Allow acces to environment Variables
 const port = process.env.PORT;
 //Allows our express server to be able to handle JSON data 
 app.use(express.json());
 //Hosts my Static HTML and CSS files
 app.use('/', express.static('public'))
 
-
-
-//Authantication End Point 
+//headers.append('Content-Type', 'text/json');
 app.get('/token',(req,res)=>{
-	
+	//Authantication End Point
 	let url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
 	let username = process.env.CONSUMER_KEY;
 	let password = process.env.CONSUMER_SECRET;
@@ -30,6 +28,56 @@ app.get('/token',(req,res)=>{
 	//.done()
 })
 
+
+app.post('/lipa',(req,res)=>{
+	let url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
+	let {access_token,PhoneNumber} = req.body;
+	let Timestamp= moment().format('YYYYMMDDHHmmss');
+	let ShortCode = process.env.SHORT_CODE;
+	let PassKey = process.env.PROCESS_PASSKEY;
+	let Password = new Buffer.from(ShortCode+PassKey+Timestamp).toString('base64')
+	let Amount ="10";//This is just a place hoder
+	let bearer = 'Bearer ' + access_token;
+	let AccountReference ="SiJothamPS";
+	let TransactionDesc="test"
+	let request = {
+		BusinessShortCode: ShortCode,
+		Password,
+		Timestamp,
+		TransactionType: "CustomerPayBillOnline",
+		Amount,
+		PartyA: PhoneNumber,
+		PartyB:ShortCode,
+		PhoneNumber,
+		CallBackURL: process.env.CALL_BACK_URL,
+		AccountReference,
+		TransactionDesc
+	  }
+	let body = JSON. stringify(request)
+	console.log(body)
+	fetch(url, {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Authorization': bearer,
+			'Content-Type': 'application/json'
+		},
+		body
+	}).then(response => response.json())
+	  .then(data => res.json(data))
+})
+
+
+
+
+//Web hooks for Mpesa to verify trasaction is successful
+app.post('/hooks/mpesa',(req,res)=>{
+	let message = {
+	  "ResponseCode": "00000000",
+	  "ResponseDesc": "success"
+	};
+	res.json(message)
+})
 
 app.listen(port,() =>{
 	console.log(`Listening http://localhost:${port}`)
